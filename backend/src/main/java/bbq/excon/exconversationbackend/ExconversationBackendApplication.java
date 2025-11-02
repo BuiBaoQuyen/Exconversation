0f3393fe87af5573b4ebe0f911542a7bcbc858dd
Bui Bao Quyen <buibaoquyen012@gmail.com>
2025-11-02 16:15:38 +0700

    create backend spring

---
+++ backend/src/main/java/bbq/excon/exconversationbackend/ExconversationBackendApplication.java
@@ -0,0 +1,13 @@
+package bbq.excon.exconversationbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExconversationBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExconversationBackendApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/bbq/excon/exconversationbackend/ExconversationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package bbq.excon.exconversationbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExconversationBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
